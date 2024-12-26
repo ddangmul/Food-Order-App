1bcaa92b9f99649df3d4ee42ddd6e710f0c044be
ddangmul <j.ddangmul@gmail.com>
2024-12-26 17:42:16 +0900

    가격 숫자를 통화 형식으로 변경

---
+++ src/util/formatting.js
@@ -0,0 +1,9 @@
+export const currencyFormatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
+// export const currencyFormatter = new Intl.NumberFormat("ko-KR", {
+//   style: "currency",
+//   currency: "KRW",
+// });

6e4d107697fb3dbff446773505c856415dd4bea3
ddangmul <j.ddangmul@gmail.com>
2024-12-26 22:21:50 +0900

    UserProgress 컨텍스트 & Cart.jsx 생성해 모달창 열기

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -6,10 +6,14 @@ export default function Modal({ children, open, className = "" }) {
 
   // 백드롭을 위해 open속성을 dialog에 설정하지 않고, useEffect에서 showModal() 사용
   useEffect(() => {
+    const modal = dialog.current;
     if (open) {
       // 여기서 dialog 요소에 접근하기 위해 참조 사용
-      dialog.current.showModal();
+      modal.showModal();
     }
+
+    // clean up 함수: 컴포넌트 재실행 전, open이 false일 경우 실행
+    return () => modal.close();
   }, [open]);
 
   return createPortal(

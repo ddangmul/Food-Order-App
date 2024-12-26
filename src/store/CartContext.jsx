333a95cb3ade70fec9d567b6fa6e911ad09b97a9
ddangmul <j.ddangmul@gmail.com>
2024-12-26 19:09:26 +0900

    CartContext & Reducer : REMOVE_ITEM, reducer 완성 및 컨텍스트 활용

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -34,13 +34,51 @@ function cartReducer(state, action) {
 
   if (action.type === "REMOVE_ITEM") {
     // 메뉴 삭제 상태 업데이트
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    const updatedItems = [...state.item];
+
+    if (existingCartItem.quantity === 1) {
+      // 항목을 삭제
+      updatedItems.splice(existingCartItemIndex, 1);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity - 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return { ...state, items: updatedItems };
   }
+  return state;
 }
 
 export function CartContextProvider({ children }) {
-  useReducer(cartReducer, { items: [] });
+  const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
 
-  return <CartContext.Provider>{children}</CartContext.Provider>;
+  function addItem(item) {
+    dispatchCartAction({ type: "ADD_ITEM", item });
+  }
+
+  function removeItem(id) {
+    dispatchCartAction({ type: "REMOVE_ITEM", id });
+  }
+
+  const cartContext = {
+    items: cart.items,
+    addItem,
+    removeItem,
+  };
+
+  // console.log(cartContext);
+
+  return (
+    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
+  );
 }
 
 export default CartContext;

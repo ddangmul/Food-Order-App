204ebd473ae37b67f17da5632b3518f7e63c7b06
ddangmul <j.ddangmul@gmail.com>
2024-12-26 23:25:08 +0900

    Form 제출 & 유효성검증 & 데이터 추출

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -20,9 +20,18 @@ export default function Checkout() {
     userProgressCtx.hideCheckout();
   }
 
+  function handleSubmit(event) {
+    event.preventDefault(); //
+
+    // 데이터 추출
+    const fd = new FormData(event.target);
+    // fd.get('fulll-name')
+    const customerData = Object.fromEntries(fd.entries());
+  }
+
   return (
     <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Totla Amount: {currencyFormatter.format(cartTotal)}</p>
         <Input label="Full Name" type="text" id="full-name" />
